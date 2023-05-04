f96305c34ad237c9bdd08908da9411ba62ddc17a
Lukas1h <Lukas1h7@gmail.com>
2023-05-03 21:32:21 -0700

    Initial Commit

---
+++ BeRealRewrite/BeRealRewriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealRewriteApp.swift
+//  BeRealRewrite
+//
+//  Created by Lukas Hahn on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BeRealRewriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
